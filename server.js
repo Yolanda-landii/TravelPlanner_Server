2345704436301b1cd66b6d758820fab1363f5bab
Yolanda-landii <landiimabotja2000@gmail.com>
2025-01-14 10:20:00 +0200

    added files

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/destinations', require('./routes/destinationRoute'));
+app.use('/api/weather', require('./routes/weatherRoute'));
+app.use('/api/activities', require('./routes/ActivitiesRoute'));
+app.use('/api/favorites', require('./routes/weatherRoute'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
